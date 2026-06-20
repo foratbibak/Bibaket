ea44d7ec43b48fd6ba616ab2d2f79441c4d232cf
ForatBibak <foratbibak341@gmail.com>
2026-06-20 19:42:01 +0330

    #

--- Bibaket.Web/Areas/Admin/Controllers/UsersController.cs
+++ Bibaket.Web/Areas/Admin/Controllers/UsersController.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace Bibaket.Web.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    //[Area("Admin")]
     public class UsersController : Controller
     {
         private readonly EshopDbContext _context;
