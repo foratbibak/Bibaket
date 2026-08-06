9f3cdc08cec5c97e39d959b912d51cbbe78222a1
ForatBibak <foratbibak341@gmail.com>
2026-08-06 16:32:45 +0330

    CategoryViewModel

---
+++ Bibaket.Domin/ViewModels/Categories/CategoryFilterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Bibaket.Domain.Enums.User;
+using Bibaket.Domain.ViewModels.Common;
+using Bibaket.Domain.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bibaket.Domain.ViewModels.Categories
+{
+    public class CategoryFilterViewModel : BasePaging<CategoryViewModel>
+    {
+ 
+
+            [Display(Name = "وضعیت حذف")]
+            public FilterDeleteStatus DeleteStatus { get; set; }
+
+        
+    }
+}
---
+++ Bibaket.Domin/ViewModels/Categories/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bibaket.Domain.ViewModels.Categories
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public int? ParentId { get; set; }
+        [Display(Name ="عنوان")]
+        public string Title { get; set; }
+
+        public string? ImageName { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
